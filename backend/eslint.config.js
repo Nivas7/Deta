62b1fbf21dc054d211979ef6aca27549af92b22b
Nivas7 <nivasniyas24@gmail.com>
2025-02-07 18:33:08 +0530

    Setup Eslint

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,47 +1,14 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
 
+
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    files: ["**/*.{js,mjs,cjs,ts}"],
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.browser
-      },
-      parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-        project: "./tsconfig.json"
-      }
-    },
-    plugins: {
-      "@typescript-eslint": tseslint.plugin
-    },
-    rules: {
-      "no-console": "warn",
-      "no-unused-vars": "warn",
-      "no-use-before-define": ["error", {
-        functions: false,
-        classes: true,
-        variables: true
-      }],
-      "@typescript-eslint/explicit-function-return-type": ["error", {
-        allowExpressions: true,
-        allowTypedFunctionExpressions: true
-      }],
-      "@typescript-eslint/no-explicit-any": "error",
-      "@typescript-eslint/no-unused-vars": ["error", {
-        argsIgnorePattern: "^_",
-        varsIgnorePattern: "^_"
-      }],
-      "@typescript-eslint/consistent-type-imports": ["error", {
-        prefer: "type-imports"
-      }],
-      "no-multiple-empty-lines": ["error", { max: 1 }],
-      "object-curly-spacing": ["error", "always"]
-    }
-  },
-  ...tseslint.configs.recommended
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
 ];
