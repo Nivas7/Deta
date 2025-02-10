f148b7a6d1d5bef596a02766789acdf680ae3da2
Srinivas <nivasniyas24@gmail.com>
2025-02-10 11:45:07 +0530

    Setup Initial frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <main>
+      <h1 className="text-3xl">Hello from App</h1>
+      <Button>Welcome</Button>
+    </main>
+  )
+}
+
+export default App
